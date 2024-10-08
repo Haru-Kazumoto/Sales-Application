b86758e8a2c06c81088b32b4d98f91343d20ccb5
Haru-Kazumoto <jiaathaya@gmail.com>
2024-10-08 20:10:56 +0700

    add: build assets

--- resources/js/Pages/Sales/Sale/ListCO.vue
+++ resources/js/Pages/Sales/Sale/ListCO.vue
@@ -1,21 +1,53 @@
 <template>
-    <div>
+    <div class="d-flex flex-column gap-4">
+        <TitlePage title="List CO" />
 
+        <div class="card shadow-sm border-0">
+            <div class="card-body">
+                <n-data-table :columns="columns" :bordered="false" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import TitlePage from '../../../Components/TitlePage.vue';
 
 export default defineComponent({
-    setup () {
-        
+    setup() {
 
-        return {}
+        function createColumns() {
+            return [
+                {
+                    title: "#",
+                    key: 'index',
+                    render(row, index) {
+                        return index + 1;
+                    }
+                },
+                {
+                    title: "TANGGAL CO",
+                    key: "customer_order_date",
+                    render(rowData) {
+                        const storehouseData = rowData.transaction_details.find((data) => {
+                            return data.category === "Storehouse";
+                        })
+
+                        return storehouseData?.value;
+                    },
+                }
+            ]
+        }
+
+        return {
+            columns: createColumns(),
+        }
+    },
+    components: {
+        TitlePage
     }
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
