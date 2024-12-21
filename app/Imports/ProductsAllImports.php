b261328eb4c3379e3ce9494cd087d3e1487e91b7
Haru-Kazumoto <jiaathaya@gmail.com>
2024-12-21 11:48:26 +0700

    change

--- app/Imports/ProductsAllImports.php
+++ app/Imports/ProductsAllImports.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Parties;
 use App\Models\Products;
 use App\Models\ProductType;
+use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
@@ -32,24 +33,29 @@ class ProductsAllImports implements ToCollection, WithStartRow
             {
                 foreach($collection as $row) 
                 {
-                    // dd($row[7]);
                     $supplier = Parties::where('name', $row[2])->first();
                     $product_type = ProductType::where('name', $row[7])->first();
 
-                    $data = Products::create([
+                    if (!$supplier) {
+                        throw new Exception("Supplier with name {$row[2]} not found.");
+                    }
+
+                    if (!$product_type) {
+                        throw new Exception("Product type with name {$row[7]} not found.");
+                    }
+
+                    Products::create([
                         'code' => $row[3],
                         'name' => $row[4],
                         'unit' => $row[5],
                         'product_company' => $row[1],
                         'product_type' => $row[6],
                         'category' => $row[8],
-                        // 'product_type_id' => $product_type->id,
-                        //7 buat channel / segment
+                        'product_type_id' => $product_type->id,
                         'supplier_id' => $supplier->id
                     ]);
-
-                    $data->productType()->associate($product_type);
                 }
+
             }
         );
     }
