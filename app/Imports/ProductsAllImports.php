e0980c401cca9def1622ad60b9cf4fb2eaccd661
Haru-Kazumoto <jiaathaya@gmail.com>
2025-02-03 17:38:25 +0700

    feat: adding skipping feature if product has exist

--- app/Imports/ProductsAllImports.php
+++ app/Imports/ProductsAllImports.php
@@ -38,6 +38,7 @@ class ProductsAllImports implements ToCollection, WithStartRow
                     $supplier = Parties::where('name', $row[2])->first();
                     $product_type = ProductType::where('name', $row[7])->first();
                     $product_sub_type = ProductSubType::where('name', $row[6])->first();
+                    $existingProduct = Products::where('code', $row[3])->first(); // Cek kode produk
 
                     if (!$supplier) {
                         throw new Exception("Supplier with name {$row[2]} not found at row {$index}");
@@ -49,6 +50,11 @@ class ProductsAllImports implements ToCollection, WithStartRow
 
                     if (!$product_sub_type) {
                         throw new Exception("Product sub type with name {$row[6]} not found at row {$index}");
+                    }
+
+                    if ($existingProduct) {
+                        Log::info("Skipping row {$index}: Product with code {$row[3]} already exists.");
+                        continue; // Skip jika kode produk sudah ada
                     }
 
                     Products::create([
